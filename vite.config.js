8ac4c2d86e93814b378c173ee0e6edc01bca2665
Teng Liu <27rabbitlt@gmail.com>
2025-05-02 18:10:17 +0200

    support gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
+const repoName = 'my-vue-site'
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
   plugins: [
     vue(),
     vueDevTools(),
